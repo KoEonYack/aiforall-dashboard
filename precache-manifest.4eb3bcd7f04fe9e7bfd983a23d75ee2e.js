0e4a27d897d52766501bff2624748e01bda88a94
KoEonYack <21300035@handong.edu>
2020-04-11 20:50:03 +0900

    Updates

---
+++ precache-manifest.4eb3bcd7f04fe9e7bfd983a23d75ee2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd9bc13a926af0c556b45a3a60de203",
+    "url": "/github.io/aiforall-dashboard/index.html"
+  },
+  {
+    "revision": "9934c1034556db369000",
+    "url": "/github.io/aiforall-dashboard/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a29815e8254410eb32c9",
+    "url": "/github.io/aiforall-dashboard/static/css/main.6104c46b.chunk.css"
+  },
+  {
+    "revision": "9934c1034556db369000",
+    "url": "/github.io/aiforall-dashboard/static/js/2.44fe1536.chunk.js"
+  },
+  {
+    "revision": "6e71b6ee5ea7775d33431df95d229f09",
+    "url": "/github.io/aiforall-dashboard/static/js/2.44fe1536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a29815e8254410eb32c9",
+    "url": "/github.io/aiforall-dashboard/static/js/main.62eaecce.chunk.js"
+  },
+  {
+    "revision": "bcf2d22470f9103b3416",
+    "url": "/github.io/aiforall-dashboard/static/js/runtime-main.f14b4bee.js"
+  }
+]);
