cacee45a0996b2e8d39814ec79b5f6ba46956ea0
KoEonYack <21300035@handong.edu>
2020-04-11 21:01:51 +0900

    Update deploy

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React, { Component } from "react";
-import { Route } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import "./App.css";
 import HeaderPage from './pages/Header'
 import Container from './pages/Container'
-import Lectures from './pages/Lectures'
-
-import $ from 'jquery';
-window.jQuery = $;
-window.$ = $;
-
-
 
 const App = () => {
   return (
