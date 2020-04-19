179656a0f2f59679529039ad6de53e43720d1f97
KoEonYack <21300035@handong.edu>
2020-04-19 14:34:09 +0900

    Week8 Bible 교체

--- src/pages/Bible.js
+++ src/pages/Bible.js
@@ -8,17 +8,19 @@ function Bible() {
             </div>
             <div className="block-content">
                 <br />
-                <img src="https://github.com/KoEonYack/aiforall-dashboard/blob/master/src/img/week8.jpg?raw=true" width="40%"  className="bible-img" />
+                <img src="https://github.com/KoEonYack/aiforall-dashboard/blob/master/src/img/week8_2.jpg?raw=true" width="40%"  className="bible-img" />
                 <br/>
                 <center> 
                     <i className="fa fa-youtube-play youtube-icon"></i> 
-                    <a href="https://youtu.be/JEjy1udlSSw"> 김복유 밴드｜전부가 되소서 </a>
+                    <a href="https://youtu.be/fv-PDzlnG1c"> 세상에서 가장 귀한 이야기 </a>
                     <br/>
-                    <br/> 아무것도 아닌 나의 삶에 하나님께서 찾아오셨기에 하루를 살 수 있습니다.
+                    <br/> 
+
+                    하나님이 세상을 이처럼 사랑하사 독생자를 주셨으니 <br />
+                    이는 그를 믿는 자마다 멸망하지 않고 영생을 얻게 하려 하심이라 <br />
+                    (요 3:16) <br/>
                     <br/>
-                    <br/>
-                    내 삶의 순간 찰나의 인생이 <br/>
-                    주 없이 살진 않게 전부가 되소서 <br/>
+                    <a href="https://youtu.be/q388gg1Pusg">하나님의 사랑이야기, 316스토리 보러가기</a>
                     <br/>
                 </center>
 
