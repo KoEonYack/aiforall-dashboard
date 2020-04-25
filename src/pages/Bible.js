b8fb612a3b16739b3ade85e4dc11b93176840bc1
KoEonYack <21300035@handong.edu>
2020-04-25 14:07:39 +0900

    Week9 bible update

--- src/pages/Bible.js
+++ src/pages/Bible.js
@@ -8,19 +8,18 @@ function Bible() {
             </div>
             <div className="block-content">
                 <br />
-                <img src="https://github.com/KoEonYack/aiforall-dashboard/blob/master/src/img/week8_2.jpg?raw=true" width="40%"  className="bible-img" />
+                <img src="https://github.com/KoEonYack/aiforall-dashboard/blob/master/src/img/week9.jpg?raw=true" width="40%"  className="bible-img" />
                 <br/>
                 <center> 
                     <i className="fa fa-youtube-play youtube-icon"></i> 
-                    <a href="https://youtu.be/fv-PDzlnG1c"> 세상에서 가장 귀한 이야기 </a>
+                    <a href="https://youtu.be/ULBJ8BQ4gGQ"> [영화 교회오빠 OST] 이원진 - 욥의고백 </a>
                     <br/>
                     <br/> 
-
-                    하나님이 세상을 이처럼 사랑하사 독생자를 주셨으니 <br />
-                    이는 그를 믿는 자마다 멸망하지 않고 영생을 얻게 하려 하심이라 <br />
-                    (요 3:16) <br/>
+                        내가 말하겠사오니 주는 들으시고 내가 주께 묻겠사오니 주여 내게 알게 하옵소서 <br />
+                        내가 주께 대하여 귀로 듣기만 하였사오나 이제는 눈으로 주를 뵈옵나이다 <br />
+                        그러므로 내가 스스로 거두어들이고 티끌과 재 가운데에서 회개하나이다 <br />
+                        (욥 42:4-6) <br />
                     <br/>
-                    <a href="https://youtu.be/q388gg1Pusg">하나님의 사랑이야기, 316스토리 보러가기</a>
                     <br/>
                 </center>
 
