ed0f44908ecaa96ca23cda6a6e01934415d64ee1
KoEonYack <21300035@handong.edu>
2020-05-02 13:02:29 +0900

    Update week10 bible

--- src/pages/Bible.js
+++ src/pages/Bible.js
@@ -4,22 +4,22 @@ function Bible() {
     return (
         <div>
             <div className="block-header">
-                <div className="block-title">Week 9 Message</div>
+                <div className="block-title">Week 10 Message</div>
             </div>
             <div className="block-content">
                 <br />
-                <img src="https://github.com/KoEonYack/aiforall-dashboard/blob/master/src/img/week9.jpg?raw=true" width="40%"  className="bible-img" />
+                <img src="https://github.com/KoEonYack/aiforall-dashboard/blob/master/src/img/week10.jpg?raw=true" width="40%"  className="bible-img" />
                 <br/>
                 <center> 
                     <i className="fa fa-youtube-play youtube-icon"></i> 
-                    <a href="https://youtu.be/ULBJ8BQ4gGQ"> [🎬 영화 교회오빠 OST] 이원진 - 욥의고백 </a>
+                    <a href="https://youtu.be/UylA-4dD3OE"> 🤵 신랑이 👰 신부를 기뻐함 같이(하희라, 최수종) </a>
                     <br/>
                     <br/>
                     <br/> 
-                        내가 말하겠사오니 주는 들으시고 내가 주께 묻겠사오니 주여 내게 알게 하옵소서 <br />
-                        내가 주께 대하여 귀로 듣기만 하였사오나 이제는 눈으로 주를 뵈옵나이다 <br />
-                        그러므로 내가 스스로 거두어들이고 티끌과 재 가운데에서 회개하나이다 <br />
-                        (욥 42:4-6) <br />
+                        너의 하나님 여호와가 너의 가운데에 계시니 그는 구원을 베푸실 전능자이시라  <br />
+                        그가 너로 말미암아 기쁨을 이기지 못하시며 너를 잠잠히 사랑하시며  <br />
+                        너로 말미암아 즐거이 부르며 기뻐하시리라 하리라가운데에서 회개하나이다 <br />
+                        (습 3:17) <br />
                     <br/>
                     <br/>
                 </center>
