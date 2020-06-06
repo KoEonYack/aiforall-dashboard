905847b858460a9ade7a5053eaa22e778e806ae9
KoEonYack <21300035@handong.edu>
2020-06-06 18:31:11 +0900

    Update Week15 message

--- src/pages/Bible.js
+++ src/pages/Bible.js
@@ -4,43 +4,36 @@ function Bible() {
     return (
         <div>
             <div className="block-header">
-                <div className="block-title">Week 14 Message</div>
+                <div className="block-title">Week 15 Final Message</div>
             </div>
             <div className="block-content">
                 <br />
-                <img src="https://scontent-ssn1-1.xx.fbcdn.net/v/t1.0-9/p960x960/89835751_1125164567833885_7436263194693206016_o.jpg?_nc_cat=102&_nc_sid=2d5d41&_nc_ohc=3kHmo2WO9FwAX_ZM7rJ&_nc_ht=scontent-ssn1-1.xx&_nc_tp=6&oh=84b713ed02344cdbf3ab6772e4032569&oe=5EF676AB" width="40%"  className="bible-img" />
-
+                <img src="https://github.com/KoEonYack/aiforall-dashboard/blob/master/src/img/week15.jpg?raw=true" width="80%"  className="bible-img" />
                 <br/>
                 <center> 
-                    <i className="fa fa-youtube-play youtube-icon"></i> 
-                    <a href="https://www.youtube.com/watch?v=JhCAb7kFy2I"> 한동의 노래 - 베드로의 고백  </a>
-                    <br /> 
-                    <br />
-                    
-                    안심하라 내니 두려워 말라<br />
-                    날 부르시는 그의 음성따라<br />
-                    걸음걸음마다 주 의지하며<br />
-                    나 믿음으로 걷네<br />
-                    <br />
-
+                    누구에게나 시작이 주어지며, 누구든지 끝을 피할 수 없습니다.
                 </center>
                 <br/>
                 <br/>
                 <br/>
-                <br/>
 
-
-                <img src="https://github.com/KoEonYack/aiforall-dashboard/blob/master/src/img/week14.PNG?raw=true" width="90%"  className="bible-img" />
+                <center>
+                하나님, 이 땅에 청년의 예배를 받아 주시옵소서. 대학생들의 찬양을 받아 주시옵소서. <br />
+                우리가 성소에서 푸른 초장에서 고요하고 경건한 예배에만 머물러 있는 것이 아니라 전쟁터의 한가운데로 달려갑니다. <br />
+                골리앗이 쓰러지는 것을 믿음으로 바라보며 여리고가 무너지는 것을 바라보며 주님을 찬양하고 예배합니다. <br />
+                우리가 승리의 함성으로 주님을 경배합니다. 캠퍼스의 부흥을 갈망하며 <br />
+                하나님을 경배하고 예배하는 이 땅에 대학생들의 예배를 통하여서 찬양을 통하여서 나라를 변화시켜 주시고 놀라운 영광과 승리가  <br />
+                이 세대 안에 이루어질 수 있도록 우리의 찬양과 예배를 받아주시옵소서 하나님은 어두움 가운에 빛을 바라시는 영광의 하나님이십니다. <br />
+                <br />
+                <a href="https://www.youtube.com/watch?v=wEq6BW5yCGo">[우리 안에 사랑을 - 예수전도단 캠퍼스워십 1집 ‎中] </a>
+                </center>
                 <br/>
-                
-                <center> TA가 작년 봄에 찍은 사진입니다.</center>
                 <br/>
                 <br/>
-                <center> 사진 속의 현동홀은 한동대의 본관이며 1995년도 한동의 탄생과 함께 처음 세워진 4동의 건물 중 하나입니다. <br />
-                    건물 정면 4층 높이 중앙에는 한동대학교의 영문 교명인 Handong Global University 가 아닌  <br />
-                    대학의 기독교적 정체성을 나타내는 ‘Handong-God’s University’가 새겨져 있습니다. </center>
-
-                <br/>
+                <center>
+                    한동에서 매일의 삶의 여정이 하나님 보시기에 합한 자들로 세워가기를 축복합니다. <br />
+                    대시보드를 이용해 주셔서 감사합니다. Fin. 
+                </center> 
                 <br/>
                 <center>
                     (GOD IS GOOD ALL THE TIME) &lt;&gt;&lt; <a href="https://ko.wikipedia.org/wiki/%EC%9D%B4%ED%81%AC%ED%8B%B0%EC%8A%A4">  </a>
